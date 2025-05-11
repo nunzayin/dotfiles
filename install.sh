feb7f021ec16598729f7b07cdeea728644d684a7
nz <64735893+nunzayin@users.noreply.github.com>
2025-05-11 15:45:09 +0300

    Now REALLY added yay to install script

--- install.sh
+++ install.sh
@@ -3,9 +3,13 @@
 # Workspace installation script
 # You better invoke this script via make install
 
-if [[ -n $$(command -v yay) ]]; then
+echo "Performing workspace installation..."
+
+if ! [[ -n $(command -v yay) ]]; then
+    echo "yay not found, installing..."
     sudo pacman -S --needed base-devel
     git clone "https://aur.archlinux.org/yay.git"
     cd yay
     makepkg -si
+    cd ..
 fi
