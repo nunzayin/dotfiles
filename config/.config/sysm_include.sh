f54d6f534cedad617b7dc770cfe71c9780e57099
nz <64735893+nunzayin@users.noreply.github.com>
2025-05-22 21:22:00 +0300

    Added git fetch module
    Added configs to git_fetch.sh for sysmaint

--- config/.config/sysm_include.sh
+++ config/.config/sysm_include.sh
@@ -1,6 +1,13 @@
 . $MODULES/mirrors.sh
 . $MODULES/update.sh
 . $MODULES/omz_update.sh
+
+REPOS=(
+    "$HOME/dotfiles"
+    "$HOME/util"
+)
+. $MODULES/git_fetch.sh
+
 . $MODULES/orphans.sh
 . $MODULES/pacman_cache.sh
 . $MODULES/syslogs.sh
