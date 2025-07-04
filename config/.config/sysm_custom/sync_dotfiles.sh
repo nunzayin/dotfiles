aeb8bc0157b78b520ebe63583a397874866c410a
nz <64735893+nunzayin@users.noreply.github.com>
2025-07-05 00:30:27 +0300

    Reduced custom sysm verbosity

--- config/.config/sysm_custom/sync_dotfiles.sh
+++ config/.config/sysm_custom/sync_dotfiles.sh
@@ -4,6 +4,5 @@ MODULE="sync_dotfiles"
 DEPS=("$HOME/dotfiles/sync.sh")
 
 if check_deps; then
-    stage "SYNCING CONFIG FILES"
     $HOME/dotfiles/sync.sh
 fi
--- sync.sh
+++ sync.sh
@@ -5,7 +5,7 @@
 CURRENT_DIR="$(pwd)"
 WORKDIR="$(dirname "$(realpath "$0")")"
 
-stow -Rv --adopt --dir $WORKDIR/config --target $HOME .
+stow -R --adopt --dir $WORKDIR/config --target $HOME .
 cd $WORKDIR
 git restore .
 cd $CURRENT_DIR
