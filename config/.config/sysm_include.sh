f1673455dc31f0e0c50420aa51b9d2ccc08ccd5e
nz <64735893+nunzayin@users.noreply.github.com>
2025-07-06 00:27:12 +0300

    Changed sysm modules order

--- config/.config/sysm_include.sh
+++ config/.config/sysm_include.sh
@@ -1,12 +1,12 @@
-. $MODULES/mirrors.sh
-. $MODULES/update.sh
-
 REPOS=(
     "$HOME/utils/sysmaint"
     "$HOME/utils/togglesound"
     "$HOME/dotfiles"
 )
 . $MODULES/git_pull.sh
+
+. $MODULES/mirrors.sh
+. $MODULES/update.sh
 
 . $HOME/.config/sysm_custom/sync_dotfiles.sh
 . $MODULES/orphans.sh
