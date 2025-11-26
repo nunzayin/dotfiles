86b8d95ebf8ea79fe0c3d212697fff866932a486
nz <64735893+nunzayin@users.noreply.github.com>
2025-11-26 13:05:05 +0300

    Disabled relative numbers in nvim

--- config/.config/nvim/init.lua
+++ config/.config/nvim/init.lua
@@ -2,7 +2,6 @@ vim.g.mapleader = ' '
 vim.g.maplocalleader = ' '
 vim.g.have_nerd_font = true
 vim.opt.number = true
-vim.opt.relativenumber = true
 vim.schedule(function() 
     vim.opt.clipboard = 'unnamedplus'
 end)
